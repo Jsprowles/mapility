e6e2a0e63d3adbcf9b1074b7a6bcdbe7af957c6e
Jeremiah Sprowles <jmsprowles@gmail.com>
2012-11-29 16:03:17 -0500

    theme changes

---
+++ system/cms/logs/log-2012-11-29.php
@@ -0,0 +1,50 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2012-11-29 19:08:16 --> Severity: Warning  --> Creating default object from empty value /Applications/MAMP/htdocs/mapility/system/cms/modules/themes/controllers/admin.php 172
+ERROR - 2012-11-29 19:08:16 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /Applications/MAMP/htdocs/mapility/system/codeigniter/core/Exceptions.php:186) /Applications/MAMP/htdocs/mapility/system/codeigniter/core/Output.php 368
+ERROR - 2012-11-29 19:08:16 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /Applications/MAMP/htdocs/mapility/system/codeigniter/core/Exceptions.php:186) /Applications/MAMP/htdocs/mapility/system/codeigniter/core/Output.php 368
+ERROR - 2012-11-29 19:08:16 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /Applications/MAMP/htdocs/mapility/system/codeigniter/core/Exceptions.php:186) /Applications/MAMP/htdocs/mapility/system/codeigniter/core/Output.php 368
+ERROR - 2012-11-29 19:08:16 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /Applications/MAMP/htdocs/mapility/system/codeigniter/core/Exceptions.php:186) /Applications/MAMP/htdocs/mapility/system/codeigniter/core/Output.php 368
+ERROR - 2012-11-29 19:08:16 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /Applications/MAMP/htdocs/mapility/system/codeigniter/core/Exceptions.php:186) /Applications/MAMP/htdocs/mapility/system/codeigniter/core/Output.php 368
+ERROR - 2012-11-29 19:09:35 --> Severity: Warning  --> Creating default object from empty value /Applications/MAMP/htdocs/mapility/system/cms/modules/themes/controllers/admin.php 172
+ERROR - 2012-11-29 19:09:35 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /Applications/MAMP/htdocs/mapility/system/codeigniter/core/Exceptions.php:186) /Applications/MAMP/htdocs/mapility/system/codeigniter/core/Output.php 368
+ERROR - 2012-11-29 19:09:35 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /Applications/MAMP/htdocs/mapility/system/codeigniter/core/Exceptions.php:186) /Applications/MAMP/htdocs/mapility/system/codeigniter/core/Output.php 368
+ERROR - 2012-11-29 19:09:35 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /Applications/MAMP/htdocs/mapility/system/codeigniter/core/Exceptions.php:186) /Applications/MAMP/htdocs/mapility/system/codeigniter/core/Output.php 368
+ERROR - 2012-11-29 19:09:35 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /Applications/MAMP/htdocs/mapility/system/codeigniter/core/Exceptions.php:186) /Applications/MAMP/htdocs/mapility/system/codeigniter/core/Output.php 368
+ERROR - 2012-11-29 19:09:35 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /Applications/MAMP/htdocs/mapility/system/codeigniter/core/Exceptions.php:186) /Applications/MAMP/htdocs/mapility/system/codeigniter/core/Output.php 368
+ERROR - 2012-11-29 19:36:26 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2012-11-29 19:38:23 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2012-11-29 19:44:00 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 20:25:11 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 20:25:39 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 20:25:48 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 20:25:58 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 20:31:31 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 20:32:00 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 20:36:38 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 21:01:04 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 21:01:39 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 21:03:31 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 21:05:40 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 21:06:52 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 21:07:28 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 21:08:21 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 21:10:09 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 21:10:49 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 21:22:02 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 21:22:24 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 21:23:55 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 21:24:22 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 21:26:04 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 21:26:20 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 21:32:42 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 21:34:21 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 21:36:44 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 21:38:41 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 21:39:47 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 21:41:03 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 21:41:17 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 21:42:27 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 21:42:59 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 21:43:42 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-29 21:44:14 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
