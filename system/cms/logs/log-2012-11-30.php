73bfd6ae69b9ae8b09dfbc323fe2767ef45f70fe
Jeremiah Sprowles <jmsprowles@gmail.com>
2012-11-30 13:26:04 -0500

    screen comp

---
+++ system/cms/logs/log-2012-11-30.php
@@ -0,0 +1,4 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2012-11-30 16:37:12 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
+ERROR - 2012-11-30 16:38:33 --> Page Missing: addons/default/themes/gradientblack/img/img03.gif
